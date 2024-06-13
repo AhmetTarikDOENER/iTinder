9892696b0a0a900d82a9673d51f7c7e4cfff502e
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-06-13 16:27:26 +0300

    Created an UIView extension on anchors for reduce the amount of codes when layout stackviews.

--- iTinder/ViewController.swift
+++ iTinder/ViewController.swift
@@ -1,19 +1,29 @@
-//
-//  ViewController.swift
-//  iTinder
-//
-//  Created by Ahmet Tarik DÖNER on 13.06.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.frame = CGRect(x: 0, y: 0, width: 300, height: 200)
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
-
-
 }
 
