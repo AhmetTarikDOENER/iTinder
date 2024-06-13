0f9dd0bd937f2e1e6e91f596bbd968b93293c264
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-06-13 16:50:06 +0300

    Started to built main screen by using stackview components.

--- iTinder/ViewController.swift
+++ iTinder/ViewController.swift
@@ -5,25 +5,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let redView = UIView()
-        redView.backgroundColor = .red
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let view = UIView()
+            view.backgroundColor = color
+            
+            return view
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
         let blueView = UIView()
         blueView.backgroundColor = .blue
         
-        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
-        stackView.distribution = .fillEqually
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+//        stackView.distribution = .fillEqually
         stackView.axis = .vertical
         stackView.frame = CGRect(x: 0, y: 0, width: 300, height: 200)
         view.addSubview(stackView)
         
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
+        stackView.fillSuperView()
     }
 }
 
