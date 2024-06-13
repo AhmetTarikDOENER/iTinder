2690820541cfc07583ff218b89243560eeb54563
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-06-13 14:47:09 +0300

    Started to create iTinder project

---
+++ iTinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTinder
+//
+//  Created by Ahmet Tarik DÖNER on 13.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
